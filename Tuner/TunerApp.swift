aac6bf779b3ae0aa17b310e033dffe42f5a04e93
Navemics <navemics@gmail.com>
2022-01-18 22:06:11 +0530

    Initial Commit

---
+++ Tuner/TunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TunerApp.swift
+//  Tuner
+//
+//  Created by NAVEEN MADHAN on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct TunerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
